4731bb2e99623876fccc0f0150b5e815e3625693
heshan <heshan@silentiuminternational.com.au>
2020-12-30 11:32:45 +0530

    CR:build Update

--- src/sections/service.jsx
+++ src/sections/service.jsx
@@ -13,7 +13,7 @@ render() {
     <div className="container">
       <div className="row" id="forService">
         <Controller>
-          <Scene duration={2000} offset={100} classToggle="active" triggerElement="#forService" triggerHook="onEnter" reverse={true} indicators={false}>
+          <Scene duration={1200} offset={100} classToggle="active" triggerElement="#forService" triggerHook="onEnter" reverse={true} indicators={false}>
             <div className="cr-card cr-card--cascade-top cr-card--trim cr-card--shadow-lvl3 cr-animate cr-animate--top mx-3 mx-md-0">
               <p className="cr-card__text cr-card__text--large m-0 text-center font-weight-light">Cateron is a full-service catering and event planning company that can cater for any ocation. Just hit us up, let us know how we can serve you and just step back and relax as we handle the rest.</p>
             </div>
@@ -29,7 +29,7 @@ render() {
         </div> 
         <div className="col-md-6" id="event_planing">
         <Controller>
-          <Scene duration={2000}  offset={50} classToggle="active" triggerElement="#event_planing" triggerHook="onEnter"reverse={true} indicators={false}>    
+          <Scene duration={1200}  offset={50} classToggle="active" triggerElement="#event_planing" triggerHook="onEnter"reverse={true} indicators={false}>    
               <div className="cr-card cr-card--cascade-left cr-card--trim cr-card--shadow-lvl2 cr-animate cr-animate--right"> 
                 <h1 className="cr-title mb-4">Event Planning</h1>
                   <div className="base-font--x-large">Want help to plan a memorable party/celebration with your loved one's?  Look no further, just hit us up, we'll ensure that it'll be memorable within the budget you want! </div> 
@@ -47,7 +47,7 @@ render() {
         </div> 
         <div className="col-md-6 text-md-right" id="catering">
           <Controller>
-            <Scene duration={2000}  offset={50} classToggle="active" triggerElement="#catering" triggerHook="onEnter" reverse={true} indicators={false}>    
+            <Scene duration={1200}  offset={50} classToggle="active" triggerElement="#catering" triggerHook="onEnter" reverse={true} indicators={false}>    
                 <div className="cr-card cr-card--cascade-right cr-card--trim cr-card--shadow-lvl2 order-md-0 cr-animate cr-animate--left">
                   <h1 className="cr-title mb-4">Catering</h1>
                     <div className="base-font--x-large">We specialize in cakes, pies and pastas. We also have special menues for any special occation. Send us a request with what you need and we'll get back to you</div> 
